b7268e717b8a8782781c46120820e38371bd758b
Johnnyy91 <claudiosardilli@gmail.com>
2022-11-15 17:33:43 +0100

    label and input

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,7 +3,12 @@
 @section('content')
     <form action="{{route('admin.posts.store')}}" method="POST">
         @csrf
-        
+        <label class="" for="title">Titolo:</label>
+        <input class="p-2" type="text" name="title" value='{{ old('title', '') }}'
+                    required maxlength="250" minlength="1" />
+
+        <label class="" for="content">Content:</label>
+        <textarea type="text" name="content" minlength="1" rows="30" cols="70" class="p-4">{{ old('content', '') }}</textarea>
 
     </form>
 @endsection
