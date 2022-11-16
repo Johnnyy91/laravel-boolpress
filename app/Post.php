5300a53efd6d33991226e108be9518d5a62c07de
Johnnyy91 <claudiosardilli@gmail.com>
2022-11-16 16:38:58 +0100

    elaboration categoryController

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     public $fillable = ['title', 'content', 'slug'];
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
